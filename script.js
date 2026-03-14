08abe1107016e44e0afd27d773c45e64558f0323
Rohan317685 <dsairohan@gmail.com>
2026-03-14 13:49:24 +0000

    Fo

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 let count = 0;
 
-const button = document.getElementById("clicker")
+const button = document.getElementById("clicker-btn");
+const countDisplay = document.getElementById("click-count");
+
+button.addEventListener("click", function () {
+  count++;
+
+  if (count === 1) {
+    button.textContent = `Clicked ${count} time!`;
+  } else {
+    button.textContent = `Clicked ${count} times!`;
+  }
+
+  countDisplay.textContent = count;
+});
